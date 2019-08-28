b475ec3f262cb16a070cda98dec91ef4b7aab035
unknown <gurufrongz@gmail.com>
2019-08-28 10:43:51 +0700

    Updates

---
+++ precache-manifest.643bd535c545e555c23aedec93449761.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "541dcbc1f06df293e3d6b1ee5d1be4d9",
+    "url": "/react-web/index.html"
+  },
+  {
+    "revision": "35438f42830e5db2def5",
+    "url": "/react-web/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "0993b294090d2fc57e2f",
+    "url": "/react-web/static/css/main.84d1c819.chunk.css"
+  },
+  {
+    "revision": "35438f42830e5db2def5",
+    "url": "/react-web/static/js/2.c0d80864.chunk.js"
+  },
+  {
+    "revision": "0993b294090d2fc57e2f",
+    "url": "/react-web/static/js/main.8d124f13.chunk.js"
+  },
+  {
+    "revision": "0f52fd6cbfa498b1659a",
+    "url": "/react-web/static/js/runtime~main.3869524a.js"
+  }
+]);
